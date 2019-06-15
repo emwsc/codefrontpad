ab3d4fedf7f375ab54b8512776be2645a66a399a
Uriy <eatmewhilesocold@gmail.com>
2019-06-15 20:26:38 +0300

    Добавил тесты

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import { css } from "styled-components";
 
 export const buttonStyles = css`
   background: #82204a;
---
+++ src/components/Codefrontpad/Events/Event/__tests__/index.test.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import "jest-styled-components";
+
+import Event from "../index";
+import { StyledDetails, StyledShowDetails } from "../styled";
+
+describe("render testing", () => {
+  test("render", () => {
+    const event = {
+      id: 0,
+      name: "name",
+      url: "url",
+      starts_at: new Date(2019, 7, 15, 19, 0),
+      description_short: "description_short",
+      description_html: "description_html"
+    };
+    const wrapper = shallow(<Event event={event} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("render without description html", () => {
+    const event = {
+      id: 0,
+      name: "name",
+      url: "url",
+      starts_at: new Date(2019, 7, 15, 19, 0),
+      description_short: "description_short"
+    };
+    const wrapper = shallow(<Event event={event} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("description_html max-height 25px for isDetailsExpanded eq false", () => {
+    const event = {
+      id: 0,
+      name: "name",
+      url: "url",
+      starts_at: new Date(2019, 7, 15, 19, 0),
+      description_short: "description_short",
+      description_html: "description_html"
+    };
+    const wrapper = mount(<Event event={event} />);
+    const details = wrapper.find(StyledDetails);
+    expect(details).toHaveLength(1);
+    expect(wrapper.find(StyledDetails).first()).toHaveStyleRule(
+      "max-height",
+      "25px"
+    );
+  });
+
+  it("description_html full height for isDetailsExpanded eq true", () => {
+    const event = {
+      id: 0,
+      name: "name",
+      url: "url",
+      starts_at: new Date(2019, 7, 15, 19, 0),
+      description_short: "description_short",
+      description_html: "description_html"
+    };
+    const wrapper = mount(<Event event={event} />);
+
+    const submitButton = wrapper.find(StyledShowDetails);
+    submitButton.simulate("click");
+
+    const details = wrapper.find(StyledDetails);
+    expect(details).toHaveLength(1);
+    expect(wrapper.find(StyledDetails).first()).not.toHaveStyleRule(
+      "max-height",
+      "25px"
+    );
+  });
+});
